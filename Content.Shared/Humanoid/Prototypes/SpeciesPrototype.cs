fdab25f17eed8d9e68c2c5a3704591be0b5ab6cd
sh4zik <186760089+sh4zik@users.noreply.github.com>
2025-08-27 01:26:33 +0200

    fixes

--- Content.Shared/Humanoid/Prototypes/SpeciesPrototype.cs
+++ Content.Shared/Humanoid/Prototypes/SpeciesPrototype.cs
@@ -91,10 +91,10 @@ public sealed partial class SpeciesPrototype : IPrototype
 
     // Corvax-LastnameGender-Start: Split lastname field by gender
     [DataField]
-    public string MaleLastNames { get; private set; } = "names_last_male";
+    public string MaleLastNames { get; private set; } = "NamesHumanLastMale";
 
     [DataField]
-    public string FemaleLastNames { get; private set; } = "names_last_female";
+    public string FemaleLastNames { get; private set; } = "NamesHumanLastFemale";
     // Corvax-LastnameGender-End
 
     [DataField]
