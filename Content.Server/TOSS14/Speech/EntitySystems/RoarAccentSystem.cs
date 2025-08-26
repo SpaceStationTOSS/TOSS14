eb63af1f6ace16ac14c8ba43cd58a4469875251b
sh4zik <186760089+sh4zik@users.noreply.github.com>
2025-08-26 22:55:00 +0200

    upstream

--- Content.Server/TOSS14/Speech/EntitySystems/RoarAccentSystem.cs
+++ Content.Server/TOSS14/Speech/EntitySystems/RoarAccentSystem.cs
@@ -1,12 +1,16 @@
 using System.Text.RegularExpressions;
 using Content.Server.Speech.Components;
-using Robust.Shared.Random;
+using Content.Shared.Speech;
 
 namespace Content.Server.Speech.EntitySystems;
 
 public sealed class RoarAccentSystem : EntitySystem
 {
-    [Dependency] private readonly IRobustRandom _random = default!;
+    private static readonly Regex RegexLowerS = new("s+");
+    private static readonly Regex RegexUpperS = new("S+");
+    private static readonly Regex RegexInternalX = new(@"(\w)x");
+    private static readonly Regex RegexLowerEndX = new(@"\bx([\-|r|R]|\b)");
+    private static readonly Regex RegexUpperEndX = new(@"\bX([\-|r|R]|\b)");
 
     public override void Initialize()
     {
@@ -18,17 +22,17 @@ public sealed class RoarAccentSystem : EntitySystem
     {
         var message = args.Message;
 
-        // roarrr
-        message = Regex.Replace(message, "r+", "rrr");
-        // roarRR
-        message = Regex.Replace(message, "R+", "RRR");
+        // hissss
+        message = RegexLowerS.Replace(message, "sss");
+        // hiSSS
+        message = RegexUpperS.Replace(message, "SSS");
+        // ekssit
+        message = RegexInternalX.Replace(message, "$1kss");
+        // ecks
+        message = RegexLowerEndX.Replace(message, "ecks$1");
+        // eckS
+        message = RegexUpperEndX.Replace(message, "ECKS$1");
 
-        // р => ррр
-        message = Regex.Replace(
-            message,
-            "р+",
-            _random.Pick(new List<string>() { "рр", "ррр" })
-        );
         args.Message = message;
     }
 }
